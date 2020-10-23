87af8a5405407e082a3eaae9ce56d3ea3521404b
Jaroslav Procházka <jaroslav.prossv@gmail.com>
2020-10-23 20:20:22 +0200

    working on FE

--- views/index.php
+++ views/index.php
@@ -1,10 +1,97 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title>Zlý Časy</title>
+</head>
+<body>
 
-<?php
-    include_once __DIR__ . "/Components/header.php"
-?>
 
-<h1>index page</h1>
+    <header>
+        
+        <nav class="navbar navbar-expand-lg" id="top-nav">
+            <img src="../public/assets/drunk.jpg" class="hidden-xs" id="image-top">
+            <h1 class="nav-title">Zlý Časy</h1>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target" id="toggle-button">
+                <span class="navbar-toggler-icon" id="toggle-image"></span>
+            </button>       
+            <div class="collapse navbar-collapse" id="collapse_target">
+                <ul class="navbar-nav" id="navbar-items">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">O nás</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Aktuality</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Jídelák</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nedomrdek</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nedomrdek2</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="top-line"></div>  
+            <button id="button-reservation">REZERVACE</button>
+            
+        </nav>
+           
+        <img src="../public/assets/top_pub1.jpg" alt="front1" id="image1" class="img-fluid">
+    </header>
 
-<?php
-    include_once __DIR__ . "/Components/footer.php"
-?>
+    <div class="wrapper">
+        <section class="first_view">
+            <h1>Největší výběr piv v Praze</h1>
+        </section>
+
+        <section class="daily_offers">
+            <h1>Denní nabídka piv</h1>
+            <div class="inner">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </section>
+
+        <section class="menu">
+            <div class="inner">
+                <h2>Jídelní lístek</h2>
+            </div>
+        </section>
+
+        <section class="chess_grid">
+            <div class="inner">
+                <div class="chess1">
+                    <img src="" alt="">
+                </div>
+                <div class="chess2">
+                    <h2>Obrovský výběr piv</h2>
+                </div>
+                <div class="chess3">
+                    <img src="" alt="">
+                </div>                    
+                <div class="chess4"></div>
+            </div>
+        </section>
+
+        <footer class="the_end">
+            <div class="inner">
+                        
+
+            </div>
+
+        </footer>
+
+
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js" integrity="sha512-/DXTXr6nQodMUiq+IUJYCt2PPOUjrHJ9wFrqpJ3XkgPNOZVfMok7cRw6CSxyCQxXn6ozlESsSh1/sMCTF1rL/g==" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+</body>
+</html>
