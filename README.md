4f57d06fee97cb58fc7c12ff12a8e13051bd3837
David Dvořák <dvorakdavid7@gmail.com>
2020-10-24 01:49:36 +0200

    front page update

---
+++ README.md
@@ -0,0 +1,12 @@
+# Zlý Časy
+---
+
+### instalation
+
+1) install all dependencies in public folder
+
+
+```bash
+cd public
+npm install
+```
--- views/index.php
+++ views/index.php
@@ -1,97 +1,69 @@
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/css/style.css">
-    <title>Zlý Časy</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="public/scss/main.css">
+    <!-- Custome CSS -->
+    <link rel="stylesheet" href="public/scss/index.css">
+    <title>Zlý časy</title>
 </head>
 <body>
+<style>
 
-
-    <header>
-        
-        <nav class="navbar navbar-expand-lg" id="top-nav">
-            <img src="../public/assets/drunk.jpg" class="hidden-xs" id="image-top">
-            <h1 class="nav-title">Zlý Časy</h1>
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target" id="toggle-button">
-                <span class="navbar-toggler-icon" id="toggle-image"></span>
-            </button>       
-            <div class="collapse navbar-collapse" id="collapse_target">
-                <ul class="navbar-nav" id="navbar-items">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">O nás</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Aktuality</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Jídelák</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Nedomrdek</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Nedomrdek2</a>
-                    </li>
-                </ul>
+</style>
+    <div class="">
+        <!-- Navbar flexbox (align-items-end) -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-black d-flex align-items-end">
+            <!-- clickable brend -->
+            <a class="navbar-brand" href="#">
+                <!-- flexbox -->
+                <div class="d-flex flex-row align-items-end">
+                    <img src="/public/assets/drunk.jpg" alt="" style="width: 30%;">
+                    <h3 class="logo">Zlý časy</h3>
+                </div>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                    <a class="nav-item nav-link" href="#">Pricing</a>
+                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                </div>
             </div>
-            <div id="top-line"></div>  
-            <button id="button-reservation">REZERVACE</button>
-            
+            <button type="button" class="align-self-end btn-reservation">Rezervace</button>
         </nav>
-           
-        <img src="../public/assets/top_pub1.jpg" alt="front1" id="image1" class="img-fluid">
-    </header>
+        
+        <!-- horizontal rule -->
+        <div style="width: 100%; height: 3px; background-color: white;"></div>
+        
+        <!-- Content section -->
+        <section>
+            <div style="position: relative; z-index: -1;">
+                <img class="img-fluid" src="/public/assets/top_pub1.jpg" alt="">
 
-    <div class="wrapper">
-        <section class="first_view">
-            <h1>Největší výběr piv v Praze</h1>
-        </section>
-
-        <section class="daily_offers">
-            <h1>Denní nabídka piv</h1>
-            <div class="inner">
-                <img src="" alt="">
-                <img src="" alt="">
-            </div>
-        </section>
-
-        <section class="menu">
-            <div class="inner">
-                <h2>Jídelní lístek</h2>
-            </div>
-        </section>
-
-        <section class="chess_grid">
-            <div class="inner">
-                <div class="chess1">
-                    <img src="" alt="">
+                <div class="thumbnail-content">
+                    <h1>Největší výběr piv v Praze</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 </div>
-                <div class="chess2">
-                    <h2>Obrovský výběr piv</h2>
-                </div>
-                <div class="chess3">
-                    <img src="" alt="">
-                </div>                    
-                <div class="chess4"></div>
             </div>
         </section>
 
-        <footer class="the_end">
-            <div class="inner">
-                        
-
+        <!-- Just lorem for test -->
+        <section>
+            <div style="height: 500px;">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corporis a, numquam distinctio cupiditate aspernatur neque quos? Ex, deleniti vitae!</p>
             </div>
-
-        </footer>
-
-
+        </section>
     </div>
 
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js" integrity="sha512-/DXTXr6nQodMUiq+IUJYCt2PPOUjrHJ9wFrqpJ3XkgPNOZVfMok7cRw6CSxyCQxXn6ozlESsSh1/sMCTF1rL/g==" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    
 </body>
 </html>
