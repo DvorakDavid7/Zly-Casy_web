e7fc7c10c9d8b5ff815c44b2e87873492a319425
David Dvořák <dvorakdavid7@gmail.com>
2020-10-28 09:28:38 +0100

    fron page parallax

--- views/index.php
+++ views/index.php
@@ -15,7 +15,11 @@
 <style>
 
 </style>
-    <section class="thumbnail">
+    <section class="parallax-window" 
+        data-parallax="scroll" 
+        data-speed="0.5" 
+        data-image-src="/public/assets/top_pub1.jpg">
+
         <!-- Navbar flexbox (align-items-end) -->
         <nav class="navbar navbar-expand-md navbar-dark bg-black d-flex align-items-end">
             <!-- clickable brend -->
@@ -46,15 +50,23 @@
             <p>Lorem ipsum dolor sit amet.</p>
         </div>
     </section>
+
     
     <!-- Content section -->
     <section>
-        <div style="height:800px;">
-
+        <div class="wrapper">
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates unde modi sed numquam explicabo delectus, harum quod. Est quisquam quis ratione qui voluptatum molestiae illo necessitatibus obcaecati sed dignissimos eius voluptates, facilis architecto, cupiditate, laboriosam earum quidem! Rerum veniam, aut sunt adipisci ut excepturi iure, omnis autem aperiam, a ea.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti provident asperiores neque delectus voluptatum eveniet inventore corrupti fugiat velit perspiciatis reiciendis architecto accusamus libero consectetur, officia sed sint sunt molestias praesentium. Quisquam alias ab rem incidunt quaerat. Dignissimos quae nesciunt est fuga veniam at voluptate, ab vitae, eos molestiae tempore, facere minus consectetur reprehenderit fugiat iure illum quasi deleniti porro laborum sint animi adipisci. Cupiditate, consectetur nobis reprehenderit dolores sunt ipsum blanditiis sequi facilis maxime ullam repellendus provident numquam error sint ratione vitae voluptatem laborum molestias minus accusamus mollitia est nulla. Dolores dolorum voluptatibus eveniet explicabo itaque fuga corporis unde.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti provident asperiores neque delectus voluptatum eveniet inventore corrupti fugiat velit perspiciatis reiciendis architecto accusamus libero consectetur, officia sed sint sunt molestias praesentium. Quisquam alias ab rem incidunt quaerat. Dignissimos quae nesciunt est fuga veniam at voluptate, ab vitae, eos molestiae tempore, facere minus consectetur reprehenderit fugiat iure illum quasi deleniti porro laborum sint animi adipisci. Cupiditate, consectetur nobis reprehenderit dolores sunt ipsum blanditiis sequi facilis maxime ullam repellendus provident numquam error sint ratione vitae voluptatem laborum molestias minus accusamus mollitia est nulla. Dolores dolorum voluptatibus eveniet explicabo itaque fuga corporis unde.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti provident asperiores neque delectus voluptatum eveniet inventore corrupti fugiat velit perspiciatis reiciendis architecto accusamus libero consectetur, officia sed sint sunt molestias praesentium. Quisquam alias ab rem incidunt quaerat. Dignissimos quae nesciunt est fuga veniam at voluptate, ab vitae, eos molestiae tempore, facere minus consectetur reprehenderit fugiat iure illum quasi deleniti porro laborum sint animi adipisci. Cupiditate, consectetur nobis reprehenderit dolores sunt ipsum blanditiis sequi facilis maxime ullam repellendus provident numquam error sint ratione vitae voluptatem laborum molestias minus accusamus mollitia est nulla. Dolores dolorum voluptatibus eveniet explicabo itaque fuga corporis unde.
+            
+            </p>
         </div>
     </section>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="/public/lib/parallax.js/parallax.min.js"></script>
 </body>
 </html>
